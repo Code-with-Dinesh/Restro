68eba2fbd26bb869feee91c393d7a340162852ec
Dinesh <dineshgarig@gmail.com>
2025-09-23 21:59:50 +0530

    feat: add authentication middleware for token verification and user retrieval

---
+++ Backend/middleware.js/auth.js
@@ -0,0 +1,24 @@
+import jwt from "jsonwebtoken";
+import ApiError from "../utils/ApiError.js";
+import user from "../models/user.model.js";
+
+export const authmiddleware = async(req,res,next)=>{
+   try {
+    const token = req.cookies?.accessToken || req.headers.authorization?.replace("Bearer ", "").trim()
+    console.log(token)
+    if(!token){
+        throw new ApiError(401,"Unauthorize : No token provided")
+    }
+    const decode = jwt.verify(token,process.env.ACCESS_TOKEN_KEY)
+    console.log(decode)
+
+    const myuser = await user.findById(decode._id).select("-password")
+    if(!myuser){
+        throw new ApiError(401,"Unauthorize Invalid token")
+    }
+    req.user = myuser
+    next()
+   } catch (error) {
+      next(error)
+   }
+}
