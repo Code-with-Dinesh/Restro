c9bb14e7b74bec4edeaf7744123caa25c0d65df8
Dinesh <dineshgarig@gmail.com>
2025-10-06 19:10:44 +0530

    payment model

---
+++ Backend/models/payment.model.js
@@ -0,0 +1,39 @@
+import mongoose from 'mongoose'
+
+const paymentSchema = mongoose.Schema({
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'user'
+    },
+    orderId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:'order'
+    },
+    razorpay_orderId:{
+        type:String,
+        required:true
+    },
+    razorpay_paymentId:{
+        type:String,
+        required:true
+    },
+    razorpay_signature: {
+    type: String,
+  },
+    amount:{
+        type:Number,
+        required:true
+    },
+    status:{
+        type:String,
+        enum:["paid","created","failed"],
+        default:"created"
+    },
+    createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+})
+
+const payment = mongoose.model('payment',paymentSchema)
+export default payment;
